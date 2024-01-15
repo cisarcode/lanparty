81133c90783f5ec6671607c881bf87828666e479
cisarcode <cisarcode@gmail.com>
2024-01-15 09:14:39 -0300

    Inicio de history en parte 1d

--- cisar/parte1d/src/App.jsx
+++ cisar/parte1d/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-    const { clicks, setClicks} = useState({
+    const [clicks, setClicks] = useState({
         left:0,
         right:0
     })
@@ -18,11 +18,48 @@ function App() {
     return (
         <>
             <div>{clicks.left}
-            <button onClick={handleLeftClick()} >Left</button>
+            <button onClick={handleLeftClick} >Left</button>
             </div>
             <div>{clicks.right}
-            <button onClick={handleRightClick()} >Right</button>
+            <button onClick={handleRightClick} >Right</button>
             </div>
+            <div>Varciacion</div>
+            <VariacionA/>
+        </>
+    )
+}
+
+const VariacionA = () => {
+
+    const [left , setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAllClicks] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+        setLeft(prevLeft => prevLeft + 1);
+        setAllClicks(allClicks.concat('L'))
+        setTotal(prevTotalClicks => prevTotalClicks + 1)
+    }
+
+    const handleRightClick = () =>
+    {
+        setRight(prevRight => prevRight + 1)
+        setAllClicks(allClicks.concat('R'))
+        setTotal(prevTotalClicks => prevTotalClicks + 1)
+    }
+
+
+    return(
+        <>
+            <div>{left}
+                <button onClick={handleLeftClick}> Left </button>
+            </div>
+            <div>{right}
+                <button onClick={handleRightClick}> Right </button>
+            </div>
+            <div>Todos los clics: {allClicks.join(' ')}</div>
+            <div>Total: {total}</div>
         </>
     )
 }
